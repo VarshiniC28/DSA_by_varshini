ed5330a23e84ab3c18649b32cc26fcd37d86e65f
VarshiniC28 <varshinic.parvam@gmail.com>
2025-08-24 00:13:06 +0530

    .

---
+++ DSA_in_java/DSA/src/binarysearchin2D/array/BSin2D.java
@@ -0,0 +1,37 @@
+package binarysearchin2D.array;
+
+import java.util.Arrays;
+
+public class BSin2D {
+	
+	//matrix arrays sorted in column wise and row wise as i psvm - then binary search
+	
+	static int[] search(int[][] matrix, int target) {
+		int r=0;
+		int c=matrix.length-1;
+		
+		while(r<matrix.length && c>=0) {
+			if(matrix[r][c]==target) return new int[] {r,c};
+			
+			if(matrix[r][c]<target) {
+				r++;
+			}else if(matrix[r][c]>target) {
+				c--;
+			}
+		}
+		return new int[] {-1,-1};
+	}
+
+	public static void main(String[] args) {
+		
+		int[][] arr = {
+				{10,20,30,40},
+				{15,25,35,45},
+				{28,29,37,49},
+				{33,34,38,50}
+		};
+		
+		System.out.println(Arrays.toString(search(arr,37)));
+	}
+
+}
