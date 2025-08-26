a9b4c1d40bd4937b576af051893bd63f30c8c040
VarshiniC28 <varshinic.parvam@gmail.com>
2025-08-27 00:10:55 +0530

    Cyclic Sort

---
+++ DSA_in_java/DSA/src/cyclic/sort/leetcode/MissingNumber.java
@@ -0,0 +1,41 @@
+package cyclic.sort.leetcode;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class MissingNumber {
+	
+	static int missingNumber(int[] arr) {
+		int i=0;
+		while(i<arr.length) {
+			if(arr[i]<arr.length && arr[i]!=i) {
+				swap(arr, i, arr[i]);
+			}else {
+				i++;
+			}
+		}
+		
+		for (int j = 0; j < arr.length; j++) {
+			if(arr[j]!=j) {
+				return j;
+			}
+		}
+		
+	return arr.length;
+	}
+
+	static void swap(int[] arr, int first, int sec) {
+		int temp= arr[first];
+		arr[first]  =arr[sec];
+		arr[sec] = temp;
+	}
+
+	public static void main(String[] args) {
+
+		int[] arr = {4,0,1,2};
+		System.out.print("Missing number is: ");
+		System.out.println(missingNumber(arr));
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
--- DSA_in_java/DSA/src/sorting/CyclicSort.java
+++ DSA_in_java/DSA/src/sorting/CyclicSort.java
@@ -9,8 +9,9 @@ public class CyclicSort {
 		while(i<arr.length) {
 			int correct = arr[i]-1;
 			if(arr[i]!=arr[correct]) {
-				swap(arr,i,correct);
-			}else {
+				swap(arr, i, correct);
+			}
+			else {
 				i++;
 			}
 		}
@@ -24,13 +25,9 @@ public class CyclicSort {
 	
 	public static void main(String[] args) {
 
-//		int [] arr = {4,1,3,5,2};
-//		cyclicSort(arr);
-//		System.out.println(Arrays.toString(arr));
-		
-		int x = 5;
-		x = x++;
-		System.out.println(x);
+		int [] arr = {4,1,3,5,2};
+		cyclicSort(arr);
+		System.out.println(Arrays.toString(arr));
 	}
 
 }
