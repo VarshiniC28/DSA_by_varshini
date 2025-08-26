89c99f62558c5500f541267ee97e2b88e87d4a9d
VarshiniC28 <varshinic.parvam@gmail.com>
2025-08-26 22:10:36 +0530

    .

--- DSA_in_java/DSA/src/sorting/CyclicSort.java
+++ DSA_in_java/DSA/src/sorting/CyclicSort.java
@@ -1,14 +1,36 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class CyclicSort {
 
 	static void cyclicSort(int[] arr) {
-		
+		int i=0;
+		while(i<arr.length) {
+			int correct = arr[i]-1;
+			if(arr[i]!=arr[correct]) {
+				swap(arr,i,correct);
+			}else {
+				i++;
+			}
+		}
+	}
+	
+	static void swap(int[] arr, int first, int sec) {
+		int temp = arr[first];
+		arr[first]  = arr[sec];
+		arr[sec] = temp;
 	}
 	
 	public static void main(String[] args) {
 
+//		int [] arr = {4,1,3,5,2};
+//		cyclicSort(arr);
+//		System.out.println(Arrays.toString(arr));
 		
+		int x = 5;
+		x = x++;
+		System.out.println(x);
 	}
 
 }
