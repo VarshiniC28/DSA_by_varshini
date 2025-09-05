92f3b55c2f14d704d028f71595b5866ea1d2f6fd
VarshiniC28 <varshinic.parvam@gmail.com>
2025-09-05 23:35:42 +0530

    .

--- DSA_in_java/programmingLogic/src/pattern/programming/PatternProgramming.java
+++ DSA_in_java/programmingLogic/src/pattern/programming/PatternProgramming.java
@@ -31,9 +31,68 @@ public class PatternProgramming {
 
 	int a = 5;
 	for(int i=0; i<a; i++) {
+		//spaces
+		for (int k = 0; k < a-i; k++) {
+			System.out.print(" ");
+		}
+		
 		for(int j=0; j<=i; j++) {
 			System.out.print("*");
 		}
+		System.out.println();
+	}
+	
+	System.out.println();
+	//---------------------------------------------
+	
+	//reverse left pyramid
+	int b = 4;
+	for (int i = 0; i < b; i++) {
+		for (int j = 0; j < b-i; j++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+	
+	System.out.println();
+	
+	//--------------------------------------------
+	
+//	left pyramid number
+	int c = 5;
+	for (int i = 0; i < c; i++) {
+		for (int j = 0; j <=i; j++) {
+			System.out.print(j+1 + " ");
+		}
+		System.out.println();
+	}
+	System.out.println();
+	
+	//------------------------------------
+	
+	int d = 5;
+	for(int i=0; i<d*2; i++) {
+		int totalColsInRow = i>d ? 2*d - i : i;
+		for(int j=0; j<totalColsInRow; j++) {
+			System.out.print("*");
+		}		
+		System.out.println();
+	}
+	
+	//-----------------------------------
+	
+	//diamond
+	
+	int e = 5;
+	for(int i=0; i<e*2; i++) {
+		int totalColsInRow = i>e ? 2*e - i : i;
+		int totalSpaces = n - totalColsInRow;
+		for (int k = 0; k < totalSpaces ; k++) {
+			System.out.print(" ");
+		}
+		for(int j=0; j<totalColsInRow; j++) {
+			System.out.print("* ");
+		}		
 		System.out.println();
 	}
 }
