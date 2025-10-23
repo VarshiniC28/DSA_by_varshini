54b2ba5fbed5f559f9be3f260c55f343fcaf27d9
VarshiniC28 <varshinic.parvam@gmail.com>
2025-10-24 00:18:57 +0530

    '.

---
+++ DSA_in_java/DSA/src/recursion/part7/Backtracking/MazeProb/MazeAllDirections.java
@@ -0,0 +1,15 @@
+package recursion.part7.Backtracking.MazeProb;
+
+public class MazeAllDirections {
+	
+	//Consider a maze and what are all the possible ways to move from first box to last box
+	//Can move in all directions - Up down left right
+
+	static 
+	
+	public static void main(String[] args) {
+
+		
+	}
+
+}
