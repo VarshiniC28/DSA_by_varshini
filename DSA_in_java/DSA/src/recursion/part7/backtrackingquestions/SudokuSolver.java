ea315ebb409d75675f557f5df5930425652c9101
VarshiniC28 <varshinic.parvam@gmail.com>
2025-11-03 23:33:02 +0530

    .

--- DSA_in_java/DSA/src/recursion/part7/backtrackingquestions/SudokuSolver.java
+++ DSA_in_java/DSA/src/recursion/part7/backtrackingquestions/SudokuSolver.java
@@ -2,6 +2,11 @@ package recursion.part7.backtrackingquestions;
 
 public class SudokuSolver {
 	
+	static boolean isSafe(int[][] board, int row, int col, int num) {
+		
+		return true;
+	}
+	
 	public static void main(String[] args) {
 
 		
